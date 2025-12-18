b9746f7180d149c9b6b85c3e0c7747a6bdfc7798
EthanZ666 <zhao1fanethan@gmail.com>
2025-12-18 14:27:09 -0500

    Update MenuSystem.cs

--- Assets/Scipts/MenuSystem.cs
+++ Assets/Scipts/MenuSystem.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -51,3 +52,27 @@ public class MenuSystem : MonoBehaviour
         }
     }
 }
+=======
+// using UnityEngine;
+
+// public class MenuSystem
+// {
+//     public bool isPaused = false;
+
+//     void Update()
+//     {
+//         if (Input.GetKeyDown("escape"))
+//         {
+//             Scene.Manager.LoadScene(filler);
+//             Time.timeScale = 0f;
+//             isPaused = true;
+//         }
+//     }
+
+//     public void Return_Home()
+//     {
+//         SceneManager.LoadScene(filler);
+//     }
+
+// }
+>>>>>>> Stashed changes
