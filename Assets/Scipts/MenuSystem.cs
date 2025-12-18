f59fda681ee22f3524c7dc8ddf1ba6c55b52796f
aq <qiyyunn@gmail.com>
2025-12-17 22:55:21 -0500

    Added pause game method and return to home screen method
    - when the escape button is clicked, the pause menu will show up and the game is paused
    - when the return button is clicked (to be added), the menu will go back to the gameplay happening

--- Assets/Scipts/MenuSystem.cs
+++ Assets/Scipts/MenuSystem.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class MenuSystem
 {
-    
+    public bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Scene.Manager.LoadScene(filler);
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
+
+    public void Return_Home()
+    {
+        SceneManager.LoadScene(filler);
+    }
+
 }
