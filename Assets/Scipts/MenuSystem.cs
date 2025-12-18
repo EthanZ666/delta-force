d03c673eb9f7baed8efb8e30fedf8a07e4ede3f8
Anze Yuan <Adam.az.yuan@gmail.com>
2025-12-18 14:24:27 -0500

    Changes the basic codes for MenuSystem file
    I add the basic basic codes for Menu files. I will add images to support future codes with Angela. This is only for basic.

--- Assets/Scipts/MenuSystem.cs
+++ Assets/Scipts/MenuSystem.cs
@@ -1,22 +1,53 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MenuSystem
+public class MenuSystem : MonoBehaviour
 {
-    public bool isPaused = false;
+    [Header("Pause Menu Panel (optional)")]
+    public GameObject pausePanel;
+
+
+
+    public void LoadScene(string sceneName)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+    public void Pause()
+    {
+    if (pausePanel != null)
+        pausePanel.SetActive(true);
+
+    Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+    if (pausePanel != null)
+        pausePanel.SetActive(false);
+
+    Time.timeScale = 1f;
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown("escape"))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Scene.Manager.LoadScene(filler);
-            Time.timeScale = 0f;
-            isPaused = true;
+        if (pausePanel == null) 
+            return;
+    
+        if (pausePanel.activeSelf)
+            Resume();
+        else
+            Pause();
         }
     }
-
-    public void Return_Home()
-    {
-        SceneManager.LoadScene(filler);
-    }
-
 }
