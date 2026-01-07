87a2fa522ea6496cb666e8d0ae1520bd627e8447
aq <qiyyunn@gmail.com>
2026-01-06 19:13:09 -0500

    made volume slider
    - adjustable volume

--- Assets/Scipts/Volume.cs
+++ Assets/Scipts/Volume.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class Volume : MonoBehaviour
 {
+    public Slider volumeSlider()
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        volumeSlider.value = AudioListener.volume;
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+    }
+
+    void SetVolume(float value)
+    {
+        AudioListener.volume = value;
     }
 
     // Update is called once per frame
@@ -14,3 +22,4 @@ public class Volume : MonoBehaviour
         
     }
 }
+
