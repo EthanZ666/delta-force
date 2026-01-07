85742e4a220587b3632fc626516605d333744417
EthanZ666 <zhao1fanethan@gmail.com>
2026-01-07 13:20:26 -0500

    Constructed shop code, item prefabs, and shop assets for the towers

--- Assets/Scipts/Volume.cs
+++ Assets/Scipts/Volume.cs
@@ -1,29 +1,29 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class Volume : MonoBehaviour
-{
-    public Slider volumeSlider()
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    // Made with the help of chatgpt
-        float volumeAmount = PlayerPrefs.GetFloat("volume", 1f);
-        AudioListener.volume = volumeAmount;
-        volumeSlider.value = AudioListener.volume;
+// public class Volume : MonoBehaviour
+// {
+//     public Slider volumeSlider()
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+//     // Made with the help of chatgpt
+//         float volumeAmount = PlayerPrefs.GetFloat("volume", 1f);
+//         AudioListener.volume = volumeAmount;
+//         volumeSlider.value = AudioListener.volume;
         
-        volumeSlider.onValueChanged.AddListener(SetVolume);
-    }
+//         volumeSlider.onValueChanged.AddListener(SetVolume);
+//     }
 
-    void SetVolume(float value)
-    {
-        AudioListener.volume = value;
-        PlayerPrefs.SetFloat("volume", value);
-    }
+//     void SetVolume(float value)
+//     {
+//         AudioListener.volume = value;
+//         PlayerPrefs.SetFloat("volume", value);
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+// }
 
