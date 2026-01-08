cb1d3a42ba3b3d4c5473b3e553a894d3b705298c
aq <qiyyunn@gmail.com>
2026-01-07 23:47:44 -0500

    made music player
    - will play music in the background of the game

--- Assets/Scipts/MusicPlayer.cs
+++ Assets/Scipts/MusicPlayer.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
+    public AudioSource musicPlayer;
+    public AudioClip[] songs;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  
+    public void PlayMusic(int index)
     {
-        
-    }
+        if (index < 0 || index >= songs.Length)
+            return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        musicPlayer.clip = songs[index];
+        musicPlayer.Play();
     }
-}
