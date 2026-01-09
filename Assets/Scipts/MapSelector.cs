9bbf73046c91d4931b02fdb80297bc8d5f273a4f
aq <qiyyunn@gmail.com>
2026-01-08 23:35:37 -0500

    made map selector
    - allows player to pick from maps by clicking buttons

--- Assets/Scipts/MapSelector.cs
+++ Assets/Scipts/MapSelector.cs
@@ -1,16 +1,38 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class MapSelector : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+// public class MapSelector : MonoBehaviour
+// {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//     public MapData[] maps;
+//     public Button[] mapButtons;
+    
+//     public class MapSettings
+//     {
+//         public string mapName;
+//         public string mapDescription;
+//         public Sprite mapImage;
+//     }
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+//         DisplayButtons();
+//     }
+    
+//     void DisplayMap (string mapName)
+//     {
+//         SceneManager.LoadScene(mapName);
+//     }
+
+//         void SetupButtons()
+//     {
+//         for (int i = 0; i < mapButtons.Length; i++)
+//         {
+//             int index = i;
+
+//             mapButtons[i].interactable = true;
+
+//             mapButtons[i].onClick.RemoveAllListeners();
+//             mapButtons[i].onClick.AddListener(() =>
+//             {
+//                 DisplayMap(maps[index].mapName);
+//             });
