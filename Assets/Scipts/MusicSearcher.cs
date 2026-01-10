eecbbb73f207e235987f583f11e923293c13a2b5
aq <qiyyunn@gmail.com>
2026-01-09 23:42:12 -0500

    commented out everything
    - not finished, so i don't want it to affect the game running

--- Assets/Scipts/MusicSearcher.cs
+++ Assets/Scipts/MusicSearcher.cs
@@ -1,26 +1,26 @@
-using UnityEngine;
+// using UnityEngine;
 
-public AudioClip BinarySearch(string searchName)
-{
-    int left = 0;
-    int right = songs.Length - 1;
+// public AudioClip BinarySearch(string searchName)
+// {
+//     int left = 0;
+//     int right = songs.Length - 1;
 
-    searchName = searchName.ToLower();
+//     searchName = searchName.ToLower();
 
-    while (left <= right)
-    {
-        int mid = (left + right) / 2;
-        string midName = songs[mid].name.ToLower();
+//     while (left <= right)
+//     {
+//         int mid = (left + right) / 2;
+//         string midName = songs[mid].name.ToLower();
 
-        int comparison = string.Compare(midName, searchName);
+//         int comparison = string.Compare(midName, searchName);
 
-        if (comparison == 0)
-            return songs[mid]; 
-        else if (comparison < 0)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+//         if (comparison == 0)
+//             return songs[mid]; 
+//         else if (comparison < 0)
+//             left = mid + 1;
+//         else
+//             right = mid - 1;
+//     }
 
-    return null; 
-}
+//     return null; 
+// }
