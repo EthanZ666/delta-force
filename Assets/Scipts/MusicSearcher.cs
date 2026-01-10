c45352b6a9b637fb47f8e9dec7aa76938e626a81
aq <qiyyunn@gmail.com>
2026-01-09 23:41:36 -0500

    binary search for music tracks
    - work in process, but should search for music based on name alphabetically

--- Assets/Scipts/MusicSearcher.cs
+++ Assets/Scipts/MusicSearcher.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-public class MusicSearcher : MonoBehaviour
+public AudioClip BinarySearch(string searchName)
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    int left = 0;
+    int right = songs.Length - 1;
+
+    searchName = searchName.ToLower();
+
+    while (left <= right)
     {
-        
+        int mid = (left + right) / 2;
+        string midName = songs[mid].name.ToLower();
+
+        int comparison = string.Compare(midName, searchName);
+
+        if (comparison == 0)
+            return songs[mid]; 
+        else if (comparison < 0)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    return null; 
 }
