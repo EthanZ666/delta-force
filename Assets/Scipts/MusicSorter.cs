e6e05c1cac8f2c1dca192f00307c73e92c298d02
aq <qiyyunn@gmail.com>
2026-01-12 23:59:34 -0500

    bubble sort for music
    - based by genre

--- Assets/Scipts/MusicSorter.cs
+++ Assets/Scipts/MusicSorter.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
-public class MusicSorter : MonoBehaviour
+public void MusicBubbleSort()
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    int n = songs.Length;
 
-    // Update is called once per frame
-    void Update()
+    for (int i = 0; i < n - 1; i++)
     {
-        
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            string genreA = song[j].genre.ToLower();
+            string genreB = songs[j + 1].genre.ToLower();
+
+            if (string.Compare(genreA, genreB) > 0)
+            {
+                MusicList temp = songs[j];
+                songs[j] = songs[j + 1];
+                songs[j + 1] = temp;
+            }
+        }
     }
 }
