5a386a9e45a387b9243f5bbce35b5d13a8fcb131
EthanZ666 <zhao1fanethan@gmail.com>
2026-01-13 13:25:09 -0500

    Commented Out Error Codes

--- Assets/Scipts/AutoBootstrap.cs
+++ Assets/Scipts/AutoBootstrap.cs
@@ -1,26 +1,26 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
 
-public class AutoBootstrap : MonoBehaviour
-{
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-    private static void Init()
-    {
-//        AddIfMissing<RuntimeUIBootstrap>();
-        AddIfMissing<MusicPlayer>();
-//        AddIfMissing<GameHotkeys>();
-//        AddIfMissing<MenuSystem>();
-        AddIfMissing<MoneyManager>();
-        // 如果还有别的要挂的记得写放这里，等我file没注释了记得帮我去注释化谢谢
-    }
+// public class AutoBootstrap : MonoBehaviour
+// {
+//     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+//     private static void Init()
+//     {
+// //        AddIfMissing<RuntimeUIBootstrap>();
+//         AddIfMissing<MusicPlayer>();
+// //        AddIfMissing<GameHotkeys>();
+// //        AddIfMissing<MenuSystem>();
+//         AddIfMissing<MoneyManager>();
+//         // 如果还有别的要挂的记得写放这里，等我file没注释了记得帮我去注释化谢谢
+//     }
 
-    private static void AddIfMissing<T>() where T : Component
-    {
-        if (Object.FindObjectOfType<T>() != null) return;
+//     private static void AddIfMissing<T>() where T : Component
+//     {
+//         if (Object.FindObjectOfType<T>() != null) return;
 
-        var go = new GameObject("__" + typeof(T).Name);
-        Object.DontDestroyOnLoad(go);
-        go.hideFlags = HideFlags.HideAndDontSave;
-        go.AddComponent<T>();
-    }
-}
+//         var go = new GameObject("__" + typeof(T).Name);
+//         Object.DontDestroyOnLoad(go);
+//         go.hideFlags = HideFlags.HideAndDontSave;
+//         go.AddComponent<T>();
+//     }
+// }
