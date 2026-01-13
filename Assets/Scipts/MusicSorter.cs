3b58df838c3570e11c434adf986b5f149470ca44
aq <qiyyunn@gmail.com>
2026-01-13 00:01:24 -0500

    commented out

--- Assets/Scipts/MusicSorter.cs
+++ Assets/Scipts/MusicSorter.cs
@@ -1,22 +1,22 @@
-using UnityEngine;
+// using UnityEngine;
 
-public void MusicBubbleSort()
-{
-    int n = songs.Length;
+// public void MusicBubbleSort()
+// {
+//     int n = songs.Length;
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            string genreA = song[j].genre.ToLower();
-            string genreB = songs[j + 1].genre.ToLower();
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         for (int j = 0; j < n - i - 1; j++)
+//         {
+//             string genreA = song[j].genre.ToLower();
+//             string genreB = songs[j + 1].genre.ToLower();
 
-            if (string.Compare(genreA, genreB) > 0)
-            {
-                MusicList temp = songs[j];
-                songs[j] = songs[j + 1];
-                songs[j + 1] = temp;
-            }
-        }
-    }
-}
+//             if (string.Compare(genreA, genreB) > 0)
+//             {
+//                 MusicList temp = songs[j];
+//                 songs[j] = songs[j + 1];
+//                 songs[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
