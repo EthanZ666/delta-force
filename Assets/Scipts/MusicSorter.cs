c2ee9af1a037afa4a0c236d6253654a099d2510f
aq <qiyyunn@gmail.com>
2026-01-13 23:59:49 -0500

    exchange sort for music
    - based on duration

--- Assets/Scipts/MusicSorter.cs
+++ Assets/Scipts/MusicSorter.cs
@@ -8,7 +8,7 @@
 //     {
 //         for (int j = 0; j < n - i - 1; j++)
 //         {
-//             string genreA = song[j].genre.ToLower();
+//             string genreA = songs[j].genre.ToLower();
 //             string genreB = songs[j + 1].genre.ToLower();
 
 //             if (string.Compare(genreA, genreB) > 0)
@@ -16,6 +16,24 @@
 //                 MusicList temp = songs[j];
 //                 songs[j] = songs[j + 1];
 //                 songs[j + 1] = temp;
+//             }
+//         }
+//  }
+// public void MusicExchangeSort()
+// {
+//     int n = songs.Length;
+
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+//            
+//             if (songs[i].length > songs[j].length)
+//             {
+//             
+//                 AudioClip temp = songs[i];
+//                 songs[i] = songs[j];
+//                 songs[j] = temp;
 //             }
 //         }
 //     }
