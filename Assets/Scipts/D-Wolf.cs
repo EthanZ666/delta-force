d5db7e53fcd43ac5220adc502cd3f549132424e4
EthanZ666 <zhao1fanethan@gmail.com>
2026-01-18 22:30:09 -0500

    Continued on Inheritance
    modified D-Wolf Script by overriding the shooting class, such that there are 10% chance of getting 3 bullets that are going to be fired

--- Assets/Scipts/D-Wolf.cs
+++ Assets/Scipts/D-Wolf.cs
@@ -10,5 +10,32 @@ public class DWolf : Tower
         range = 5f;
         
     }
-    
+     protected override void Shoot()
+{
+    int bulletCount = 1;
+    float DiceNumber = Random.Range(1,11);
+
+    // 10% chance to fire 3 bullets
+    if (DiceNumber == 10)
+    {
+        bulletCount = 3;
+    }
+
+    for (int i = 0; i < bulletCount; i++)
+    {
+        GameObject projectile = Instantiate(
+            projectilePrefab,
+            firePoint.position,
+            firePoint.rotation
+        );
+
+        Projectile p = projectile.GetComponent<Projectile>();
+        if (p != null)
+        {
+            p.SetDamage(damage);
+            p.SetTarget(target);
+        }
+    }
+}
+
 }
