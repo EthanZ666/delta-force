54d64df1ac17fbe3ec42828c3f17b218a0a04329
aq <qiyyunn@gmail.com>
2026-01-19 23:55:07 -0500

    made settings buttons
    - one for turning music on/off
    - other is a slider for adjusting volume

--- Assets/Scipts/SettingScene.cs
+++ Assets/Scipts/SettingScene.cs
@@ -1,6 +1,50 @@
 using UnityEngine;
 
-public class SettingScene
+public class SettingScene : MonoBehaviour
 {
-    
+    private bool isOff
+
+    void Start()
+    {
+        float volume = PlayerPrefs.GetFloat("volume", 1f);
+        AudioListener.volume = volume;
+        if (volume == 0f)
+        {
+            isOff = true;
+        }
+        else
+        {
+            isOff = false;
+        }
+    }
+
+
+    public void ToggleOnOff()
+    {
+        isOff = !isOff;
+        if (isOff)
+        {
+            AudioListener.volume = 0f;
+            PlayerPrefs.SetFloat("volume", 0f);
+        }
+        else
+        {
+            AudioListener.volume = 1f;
+            PlayerPrefs.SetFloat("volume", 1f);
+        }
+    }
+    public void SetVolume(float value)
+    {
+        AudioListener.volume = value;
+        PlayerPrefs.SetFloat("volume", value);
+         if (volume == 0f)
+        {
+            isOff = true;
+        }
+        else
+        {
+            isOff = false;
+        }
+    }
 }
+
