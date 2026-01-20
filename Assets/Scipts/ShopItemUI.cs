86019a627cb13bd7cba9892139dd3f913e1e1557
EthanZ666 <zhao1fanethan@gmail.com>
2026-01-20 12:58:04 -0500

    Added Images for Prefab, linking the buttons with TowerPlacement Class
    Modified images for two of the tower prefabs, changed shopitem UI class for detecting clicking and respond with methods of the towerplacement class

--- Assets/Scipts/ShopItemUI.cs
+++ Assets/Scipts/ShopItemUI.cs
@@ -6,12 +6,31 @@ public class ShopItemUI : MonoBehaviour
 {
     public Image icon;
     public TMP_Text priceText;
+
     private TowerData towerData;
+    private Button button;
+    private TowerPlacementManager placement;
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+        GameObject placementObj = GameObject.FindWithTag("TowerPlacementManager");
+        TowerPlacementManager placement = placementObj.GetComponent<TowerPlacementManager>();
+
+        button.onClick.AddListener(OnClick);
+    }
 
     public void Setup(TowerData data)
     {
         towerData = data;
         icon.sprite = data.icon;
         priceText.text = data.price.ToString();
+    }
+
+    void OnClick()
+    {
+        if (placement == null || towerData == null) return;
+
+        placement.BeginPlacement(towerData);
     }
 }
