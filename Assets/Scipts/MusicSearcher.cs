05f8131f9e3f97c9beb23fffa849b12b0e25a573
aq <qiyyunn@gmail.com>
2026-01-20 23:40:21 -0500

    added comments

--- Assets/Scipts/MusicSearcher.cs
+++ Assets/Scipts/MusicSearcher.cs
@@ -2,6 +2,7 @@
 
 // public class SongSearch
 // {
+       // gets the song names from the list then sorts the names alphabetically
 //     public static List<string> GetSongNamesList(List<Song> songNames)
 //     {
 //         List<string> songNamesList = new List<string>();
@@ -15,16 +16,16 @@
 //         return songNamesList;
 //     }
 
-
+       // binary search to find the keyword the user searches within the song names
 //     public static int BinarySearch(List<string> sortedList, string keyword)
 //     {
+
 //         int left = 0;
 //         int right = sortedList.Count - 1;
 
 //         while (left <= right)
 //         {
 //             int mid = left + (right - left) / 2;
-
 
 //             int comparison = string.Compare(sortedList[mid], keyword, StringComparison.OrdinalIgnoreCase);
 
